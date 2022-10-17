94d3bf894ff6d38ae2845b084899fdee33f161d7
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-10-17 12:47:15 -0500

    login layout made, changed starting activity

---
+++ app/src/main/java/com/example/sep_socialful/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.sep_socialful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View x) {
+                openMain();
+            }
+        });
+
+    }
+
+    public void openMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
