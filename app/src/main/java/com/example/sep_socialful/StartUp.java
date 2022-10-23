9a086ecd37514cd752175cccc145a697ec7efe2a
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-10-23 14:16:01 -0500

    StartUp Created With delay

---
+++ app/src/main/java/com/example/sep_socialful/StartUp.java
@@ -0,0 +1,32 @@
+package com.example.sep_socialful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+
+        Runnable onRun = new Runnable() {
+            @Override
+            public void run() {
+                openLogin();
+            }
+        };
+        Handler h = new Handler();
+        h.postDelayed(onRun, 1600);
+
+    }
+
+    //Sends to MainActivity
+    public void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
