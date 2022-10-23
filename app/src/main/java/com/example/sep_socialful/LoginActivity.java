c41a7488d575df441ed2a88e84894931f9d4359b
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-10-22 23:30:20 -0500

    Added Register File

--- app/src/main/java/com/example/sep_socialful/LoginActivity.java
+++ app/src/main/java/com/example/sep_socialful/LoginActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private TextView registerGoTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.loginButton);
+        registerGoTo = (TextView) findViewById(R.id.textViewRegister);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View x) {
-                openMain();
-            }
+            public void onClick(View x) { openMain(); }
         });
-
+        registerGoTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View x) { openMain(); }
+        });
     }
 
+    //Sends to MainActivity
     public void openMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    //Send to RegisterActivity
+    public void openReg(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
