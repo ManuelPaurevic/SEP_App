fa45589f876abb0c5c9334ad8a3b914268d74b3e
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-10-23 13:55:52 -0500

    Finshed Registeration Activity layout

--- app/src/main/java/com/example/sep_socialful/RegisterActivity.java
+++ app/src/main/java/com/example/sep_socialful/RegisterActivity.java
@@ -2,13 +2,38 @@ package com.example.sep_socialful;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class RegisterActivity extends AppCompatActivity{
+import com.google.android.material.navigation.NavigationBarView;
+
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner genderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String sexSelected = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
