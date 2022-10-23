0900166cc07fd184e47eae3f7db77154169183e9
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-10-23 17:56:52 -0500

    Auth Enabled

--- app/src/main/java/com/example/sep_socialful/RegisterActivity.java
+++ app/src/main/java/com/example/sep_socialful/RegisterActivity.java
@@ -1,42 +1,102 @@
 package com.example.sep_socialful;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private Spinner genderSpinner;
-    private Button registerButton;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    private EditText userNicknameET;
+    private EditText userPhoneNumberET;
+    private EditText userEmailET;
+    private EditText userAgeET;
+    private EditText userPasswordET;
+    private EditText userCPasswordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        userNicknameET = (EditText) findViewById(R.id.nickNameInput);
+        userPhoneNumberET = (EditText) findViewById(R.id.phoneNumberInput);
+        userEmailET = (EditText) findViewById(R.id.emailInput);
+        userAgeET = (EditText) findViewById(R.id.ageInput);
+        userPasswordET = (EditText) findViewById(R.id.passwordRInput);
+        userCPasswordET = (EditText) findViewById(R.id.confirmPasswordRInput);
 
-        genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
+        mAuth = FirebaseAuth.getInstance();
 
+        //Sets up the Spinner
+        Spinner genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(adapter);
 
-        registerButton = (Button) findViewById(R.id.buttonReg);
-
+        Button registerButton = (Button) findViewById(R.id.buttonReg);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openLogin();
+                createNewAccount();
+                //openLogin();
             }
         });
+
+
+    }
+
+    private void createNewAccount(){
+        String email = userEmailET.getText().toString();
+        String password = userPasswordET.getText().toString();
+        String confirmPassword = userCPasswordET.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            userEmailET.setError("Email cannot be empty");
+            userEmailET.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            userPasswordET.setError("Password cannot be empty");
+            userPasswordET.requestFocus();
+        }else if (!password.equals(confirmPassword)){
+            userCPasswordET.setError("Passwords must match");
+            userCPasswordET.requestFocus();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Account Created successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Registration Error: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+
+
     }
 
     @Override
@@ -49,8 +109,8 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
     }
 
-    public void openLogin(){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
+
+
+
+
 }
