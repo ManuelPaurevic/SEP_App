9a80303ab65264ff2927458381243f4de40d7926
jasont-ran <jtran5204@gmail.com>
2022-11-13 20:32:31 -0600

    cords

--- app/src/main/java/com/example/sep_socialful/GoogleMapFragment.java
+++ app/src/main/java/com/example/sep_socialful/GoogleMapFragment.java
@@ -4,11 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,7 +21,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class GoogleMapFragment extends Fragment {
+
+    private Geocoder geocoder;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -31,11 +41,27 @@ public class GoogleMapFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-1000, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            LatLng london = getCoordsFromAddress("5314 staely ave");
+            googleMap.addMarker(new MarkerOptions().position(london).title("Marker in london"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(london));
+
         }
     };
+
+    public LatLng getCoordsFromAddress(String address){
+        LatLng coords = null;
+        Address place;
+        geocoder = new Geocoder(getActivity());
+        try{
+            List<Address> list = geocoder.getFromLocationName(address, 1);
+            place = list.get(0);
+            coords = new LatLng(place.getLatitude(), place.getLongitude());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return coords;
+    }
+
 
     @Nullable
     @Override
